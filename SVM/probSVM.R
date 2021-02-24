66b2ea1e76cabc0b7dffd3610937e5b8aa1a523d
Jaffet Leon <jaffotsa@gmail.com>
2021-02-24 17:03:33 -0600

    Create probSVM.R

---
+++ SVM/probSVM.R
@@ -0,0 +1,30 @@
+# Vamos a trabajar con un ejemplo de SVM para clasificar 
+# los datos de iris de forma LINEAL. Ojo, es importante que
+# clasifique para todas las variables contenidas en iris
+
+# 1: Cargue el paquete e1071 y haga un attach de iris
+
+# 2: Construya un conjunto de entrenamiento y otro de prueba (recomendación:
+# genere un índice binario para separar)
+
+# 3: Corra su SVM con method="C-classification" y un kernel lineal
+
+# 4: Genere una grafica de su clasificación
+
+
+########## 2do ejercicio
+
+
+# Vamos a trabajar con un ejemplo de SVM para clasificar 
+# los datos de sonar de la librería mlbench de forma LINEAL. Ojo, es importante que
+# clasifique para todas las variables contenidas en sonar
+
+# 1: Cargue el paquete e1071, mlbench) y haga un attach de sonar
+
+# 2: Construya un conjunto de entrenamiento y otro de prueba (recomendación:
+# genere un índice binario para separar)
+
+# 3: Corra su SVM con method="C-classification" y un kernel lineal
+
+# 4: Genere una grafica de su clasificación
+
