bc4282ef850febef8a70fd74a97097eca224c07e
Jaffet Leon <jaffotsa@gmail.com>
2021-02-24 17:01:54 -0600

    Create Prob.R

---
+++ Regresion/Prob.R
@@ -0,0 +1,18 @@
+# Vamos a analizar la mortalidad infantil en Espana, Somalia y Mexico. Para ello
+# realizaremos análisis de regresión lineal por MCO
+
+# 1 cargue openxlsx y abra el dataset de la clase
+
+# 2 Haga una análisis exploratorio breve de los datos
+
+# 3 Desarrolle sus modelos de regresión lineal
+
+# 4 Grafique con ggplot sus regresiones
+
+# 5 Haga un summary de sus regresiones, interprete con la teoría estadística que usted
+# domina los estadísticos t, f y el R cuadrada
+
+# 6 Opcional: haga un test durbin wattson con la paquetería lmtest usando al función
+# dwtest
+
+# 7 Haga una interpretación cualitativa de los resultados (política pública, salud pública etc.)
