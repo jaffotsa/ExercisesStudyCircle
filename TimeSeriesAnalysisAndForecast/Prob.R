ff377b9a33edea95d1f22b357057a8984bba9ace
Jaffet Leon <jaffotsa@gmail.com>
2021-02-24 17:04:37 -0600

    Create Prob.R

---
+++ TimeSeriesAnalysisAndForecast/Prob.R
@@ -0,0 +1,26 @@
+## 2. Seleccione un país en particular que sea de su agrado y. Para ello serpa necesario que utilice Excel y copie y pegue los valores de una serie de tiempo en un nuevo archivo.
+
+## 3. Cargue las siguientes librerías, importe el excel y cree un nuevo data frame con la serie del país seleccionados previamente:
+
+library(openxlsx)
+library(xts)
+library(dynlm)
+library(mFilter)
+
+## 4. Ahora cree su variable formato time series. Asignará los meses desde enero del 83 a diciembre del 2019. Use class para estar seguro que trata con un time series "ts":
+
+
+## 5.  Cree un boxplot y estudie la dispersión de datos. Anote en su respuesta su análisis respectivo:
+
+
+## 6. Haga dos correlogramas para la autocorrelación y la autocorrelación parcial. Analicelos y concluya en una respuesta breve.
+
+
+## 7. Grafique la serie temporal, y pegue ne la respuesta captura de la misma
+
+## 8. Estime el modelo aditivo o multiplicativo sea el caso. Argumente porque uso el respectivo modelo.
+
+## 9. Desagregue en sus componentes a la serie de tiempo, y explique cada componente de forma escrita en su respuesta:
+
+
+## 10. Estime por separado la tendencia, estacionalidad, valor observado y factor estócastico. Copia y pega como captura de pantalla tus graficas. Ejemplo:
